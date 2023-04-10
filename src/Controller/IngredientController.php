6733c816fef2f03a8818facc5383d1d6572fba22
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2023-04-10 09:01:11 +0200

    Added edit & delete for ingredient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -6,6 +6,7 @@ use App\Entity\Ingredient;
 use App\Form\IngredientType;
 //Importe le namespace du repository de mes ingrédients
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 //Importe Paginator Interface de Knp
 use Knp\Component\Pager\PaginatorInterface;
@@ -20,6 +21,7 @@ class IngredientController extends AbstractController
 {
     //Définis la route dans la qu'elle cette fonction seras apeller
     #[Route('/ingredient', name: 'ingredient.index', methods:['GET'])]
+    //Fonction pour afficher tous les ingrédients
     //Controller qui renvoie une Response
     //Utilise mon Repository pour les ingrédients et le nome $repository (Injection de dépendances dans les paramétres du controller)
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
@@ -38,6 +40,7 @@ class IngredientController extends AbstractController
     }
 
     #[Route('/ingredient/nouveau', name: 'ingredient.new')]
+    //Fonction pour ajouter un igrédient
     public function new(EntityManagerInterface $manager, Request $request) : Response 
     {
         //Crée un nouvel ingrédient
@@ -70,5 +73,49 @@ class IngredientController extends AbstractController
 
         //Méthode createView qui est passé au template et qui permet de crée le formulaire pour la View
         return $this->render('pages/ingredient/new.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/ingredient/edition/{id}', name: 'ingredient.edit', methods:['GET', 'POST'])]
+    //Fonction pour éditer un ingrédient
+    //Symfony regarde dans l'url le params qu'on passe et vérifie de lui même dans mon entity si un params existe et le récupére de lui même
+    public function edit( Ingredient $ingredient, Request $request, EntityManagerInterface $manager): Response
+    {
+        //Méthode du répository qui permet de trouver un igrédient par son id
+        //$ingredient = $repository->findOneBy(["id" => $id]);
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été modifié avec succès !'
+            );
+
+            return $this->redirectToRoute('ingredient.index');
+        }
+
+        return $this->render('pages/ingredient/edit.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/ingredient/suppression/{id}', name: 'ingredient.delete', methods:['GET'])]
+    //Fonction pour supprimer un ingrédient
+    public function delete(EntityManagerInterface $manager, Ingredient $ingredient): Response
+    {
+
+        //Méthode de l'entity Manager qui permet de supprimer un élément qui correspond au param passer dans l'url
+        $manager->remove($ingredient);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            'Votre ingrédient a été supprimé avec succès !'
+        );
+
+        return $this->redirectToRoute('ingredient.index');
     }
 }
